05de40192cf9aa8220b683a62f410cd49f4953df
Pravin Jadhav <pravinjadhav762@gmail.com>
2017-01-23 11:57:11 +0530

    remove server

--- server/datasources.local.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-mongodb: {
-url: process.env.DB_URL || 'mongodb://localhost:27017/blubox',
-connector: process.env.CONNECTOR || 'loopback-connector-mongodb' //https://docs.strongloop.com/display/public/LB/MongoDB+connector#MongoDBconnector-Replicasetconfiguration
-}
-};
