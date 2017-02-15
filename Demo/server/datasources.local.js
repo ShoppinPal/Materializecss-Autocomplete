beb47c656fe5898f8ab88fc37dd5be9214917cce
Pravin Jadhav <pravinjadhav762@gmail.com>
2017-02-15 18:59:19 +0530

    Changed datasource.local.js

--- Demo/server/datasources.local.js
+++ Demo/server/datasources.local.js
@@ -4,3 +4,4 @@ url: process.env.DB_URL || 'mongodb://localhost:27017/blubox',
 connector: process.env.CONNECTOR || 'loopback-connector-mongodb' //https://docs.strongloop.com/display/public/LB/MongoDB+connector#MongoDBconnector-Replicasetconfiguration
 }
 };
+//'mongodb://admin:admin@ds153239.mlab.com:53239/blubox'
