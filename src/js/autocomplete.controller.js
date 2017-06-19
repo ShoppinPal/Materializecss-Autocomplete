8c870be7cf6bdd8daba80ed40eb0dab837514219
Varun Sukheja <varun@shoppinpal.com>
2017-06-19 19:03:09 +0530

    small bug in error conditions due to null present in parentForm array

--- src/js/autocomplete.controller.js
+++ src/js/autocomplete.controller.js
@@ -502,7 +502,7 @@
          */
         self.checkError = function () {
             var isFormSubmitted = false;
-            if (self.parentForm) {
+            if (self.parentForm && self.parentForm[0] && self.parentForm[0]!==null) {
                 isFormSubmitted = self.parentForm[0].$submitted;
             }
             return self.required && !self.disableInput && !self.selectedItem && (self.isInputBlur || isFormSubmitted);
